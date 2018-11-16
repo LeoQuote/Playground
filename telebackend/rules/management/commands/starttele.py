9192bc296875c4c4b1220a3b77c155808058f016
qizhicheng <qizhicheng@cheyipai.com>
2018-11-16 20:21:14 +0800

    新增状态

--- telebackend/rules/management/commands/starttele.py
+++ telebackend/rules/management/commands/starttele.py
@@ -8,7 +8,7 @@ from rules.models import Rule, TeleUser
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
-ADMIN_LIST = ['Leocx']
+ADMIN_LIST = []
 
 def log_message(func):
     def wrapper(*args, **kwargs):
@@ -30,6 +30,7 @@ def start(bot, update):
     user = update.message.from_user 
     try:
         new_tele_user = TeleUser.objects.get(tele_id = user.id)
+        new_tele_user.status = 'start'
         response_text = "Welcome back!"
     except TeleUser.DoesNotExist:
         new_tele_user = TeleUser(
@@ -47,9 +48,57 @@ def start(bot, update):
 
     bot.send_message(chat_id=update.message.chat_id, text=response_text)
 
+@log_message
+def set_rules_entry(bot, update):
+    user = TeleUser.objects.get(tele_id = update.message.from_user.id)
+    user.status = 'rule_trigger'
+    user.save()
+    bot.send_message(chat_id=update.message.chat_id, text='已进入修改规则模式, 请输入触发语句')
 
 
-
+class text_message_handler:
+    def __init__(self, user, message):
+        self.user = user
+        self.message = message
+    
+    @property
+    def response(self):
+        return self.dispatch()
+    def on_start(self):
+        try:
+            matched_rule = Rule.objects.get(trigger_text = self.message, is_complete =True)
+            return matched_rule.response_text
+        except Rule.DoesNotExist:
+            return "没有对应规则, 试试 /set 设置一个吧"
+    def dispatch(self):
+        def func_not_found():
+            return "系统内部错误: 未找到当前{}模式下的对应方法".format(self.user.status)
+        func_name = "on_{}".format(self.user.status)
+        func = getattr(self, func_name, func_not_found)
+        return func()
+    def on_rule_trigger(self):
+        # 设置规则模式 步骤 2 输入 触发语句
+        # status: rule_trigger
+        # next_status: rule_response
+        new_rule = Rule(
+            trigger_text = self.message,
+            added_by = self.user
+        )
+        new_rule.save()
+        self.user.status = 'rule_response'
+        self.user.save()
+        return "请输入响应语句"
+    def on_rule_response(self):
+        # 设置规则模式 步骤3 输入 响应语句
+        # status: rule_response
+        # next_status: start
+        new_rule = Rule.objects.filter(added_by = self.user, is_complete=False)[0]
+        new_rule.response_text = self.message
+        new_rule.is_complete = True
+        new_rule.save()
+        self.user.status = 'start'
+        self.user.save()
+        return "规则已保存"
 @log_message
 def hello(bot, update):
     update.message.reply_text(
@@ -59,19 +108,23 @@ def hello(bot, update):
 @log_message
 def enter_rules(bot, update):
     user = TeleUser.objects.get(tele_id = update.message.from_user.id)
-    user.status = 'setting_rule_0'
+    user.status = 'rule_trigger'
     user.status_since = datetime.now()
     user.save()
     bot.send_message(chat_id=update.message.chat_id, text='已进入修改规则模式, 请输入触发语句')
 
 @log_message
-def 
-
-@log_message
 def echo(bot, update):
     if update.message.from_user.username == 'Leocx':
         bot.send_message(chat_id=update.message.chat_id, text='好的主人')
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+    u = TeleUser.objects.get(tele_id = update.message.from_user.id)
+    m = update.message.text
+    h = text_message_handler(u, m)
+    response_text = h.response 
+    if response_text:
+        bot.send_message(chat_id=update.message.chat_id, text=response_text)
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text='你难到我了...')
 
 class Command(BaseCommand):
     help = 'start telegram bot'
@@ -80,9 +133,11 @@ class Command(BaseCommand):
         updater = Updater('748269063:AAE_P3N8DD6oYABQRQk1yszDGX9nm4wVy1U',request_kwargs={'proxy_url':'http://127.0.0.1:1080'})
         start_handler = CommandHandler('start', start)
         echo_handler = MessageHandler(Filters.text, echo)
+        set_handler = CommandHandler('set', enter_rules)
         updater.dispatcher.add_handler(echo_handler)
         updater.dispatcher.add_handler(start_handler)
         updater.dispatcher.add_handler(CommandHandler('hello', hello))
+        updater.dispatcher.add_handler(set_handler)
 
         updater.start_polling()
         logging.info('Robot started')
--- telebackend/rules/models.py
+++ telebackend/rules/models.py
@@ -7,10 +7,10 @@ class TeleUser(models.Model):
     first_name = models.CharField('First Name', max_length=32)
     last_name = models.CharField('Last Name', max_length=32)
     username = models.CharField('用户名', max_length=128)
-    is_bot = models.BooleanField('是机器人吗?')
+    is_bot = models.BooleanField('是机器人吗?',default= False)
     json_data = models.CharField('json data', max_length=1024)
-    status = models.CharField('现在所处模式', max_length=32)
-    status_since = models.DateTimeField('进入模式时间')
+    status = models.CharField('现在所处模式', max_length=32, default='start')
+    status_since = models.DateTimeField('进入模式时间', auto_now=True)
     added_at = models.DateTimeField('加入时间', auto_now_add=True)
     def __str__(self):
         return self.username
@@ -22,10 +22,11 @@ class TeleUser(models.Model):
 
 class Rule(models.Model):
     trigger_text = models.CharField('触发文本', max_length=32)
-    response_text = models.CharField('响应文本', max_length=128)
-    been_triggered_for = models.IntegerField('触发次数')
-    last_triggered_at = models.DateTimeField('上次触发时间')
-    last_triggered_by = models.CharField(max_length=128)
+    response_text = models.CharField('响应文本', max_length=128, blank=True, null=True)
+    been_triggered_for = models.IntegerField('触发次数', default=0)
+    last_triggered_at = models.DateTimeField('上次触发时间', blank=True, null=True)
+    last_triggered_by = models.CharField(max_length=128, blank=True, null=True)
+    is_complete = models.BooleanField('是否完整?',default=False)
     added_by = models.CharField('添加用户', max_length=128)
     added_at = models.DateTimeField('添加时间', auto_now_add=True)
 
